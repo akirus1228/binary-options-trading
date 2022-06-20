fa3c3c4a00bd93f7cee4af053a909cd5ddbed7be
Cory Sougstad <cory@hau.me>
2022-06-20 14:19:29 -0700

    Add a couple skips where appropriate

--- apps/nft-market/src/app/hooks/use-wallet-asset.ts
+++ apps/nft-market/src/app/hooks/use-wallet-asset.ts
@@ -23,7 +23,7 @@ export const useWalletAsset = (
       asset_contract_address: contractAddress,
       limit: 1,
     },
-    { skip: !!asset }
+    { skip: !!asset || !address }
   );
 
   useGetAssetsQuery(
