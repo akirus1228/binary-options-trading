04f8b6208dad5ca47af3e589c526a11f22bb7a1b
danny-jin <icarus96119@gmail.com>
2022-08-11 21:10:00 +0900

    fix: update the backend api endpoint

--- apps/nft-market/src/app/api/backend-api.ts
+++ apps/nft-market/src/app/api/backend-api.ts
@@ -47,12 +47,8 @@ import { isDev } from "@fantohm/shared-web3";
 export const WEB3_SIGN_MESSAGE =
   "This application uses this cryptographic signature, verifying that you are the owner of this address.";
 // TODO: use production env to determine correct endpoint
-// export const NFT_MARKETPLACE_API_URL = isDev
-//   ? "https://liqd-nft-lending-test.herokuapp.com/api"
-//   : "https://liqd-nft-lending-production.herokuapp.com/api";
-
 export const NFT_MARKETPLACE_API_URL = isDev
-  ? "http://localhost:3000/api"
+  ? "https://liqd-nft-lending-test.herokuapp.com/api"
   : "https://liqd-nft-lending-production.herokuapp.com/api";
 
 export const doLogin = (address: string): Promise<LoginResponse> => {
