425f3f4c168d086e7d7edd692ba92f1a79f56ab5
recaor <redcarrot0803@gmail.com>
2022-08-19 00:17:01 -0230

    feat: ud login

--- libs/shared/web3/src/lib/constants.ts
+++ libs/shared/web3/src/lib/constants.ts
@@ -7,6 +7,12 @@ export const TOKEN_DECIMALS = 9;
 
 export const MISSING_ADDRESS = "0x0000000000000000000000000000000000000000";
 
+export const UAUTH_CLIENT_ID = "dbdd65ab-0c1e-46ad-be65-8a6adb3dd4d5";
+
+export const DEV_UD_REDIRECT_URI = "http://localhost:4200";
+
+export const PROD_UD_REDIRECT_URI = "https://beta.liqdnft.com";
+
 interface IAddresses {
   [key: number]: { [key: string]: string };
 }
