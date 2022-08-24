c99b3ad41b9b1e9b15ed67a351d558498a6952cd
krislender0104 <krislender0104@gmail.com>
2022-08-23 23:36:28 -0700

    feat: close update term dialog when user rejected

--- apps/nft-market/src/app/components/terms-form/terms-form.tsx
+++ apps/nft-market/src/app/components/terms-form/terms-form.tsx
@@ -365,6 +365,7 @@ export const TermsForm = (props: TermsFormProps): JSX.Element => {
         // user rejected signature
         // error message being dispatched from another catch
         // dispatch(addAlert({ message: "Signature rejected. Terms not updated." }));
+        props.onClose(true);
         return;
       }
 
