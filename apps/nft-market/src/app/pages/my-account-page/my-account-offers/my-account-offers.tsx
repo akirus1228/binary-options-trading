eea7ac96a463d080cda213406b79e3ca37d953b4
dev-malek <malek@fantohm.com>
2022-08-28 03:07:28 -0700

    small fix in logic

--- apps/nft-market/src/app/pages/my-account-page/my-account-offers/my-account-offers.tsx
+++ apps/nft-market/src/app/pages/my-account-page/my-account-offers/my-account-offers.tsx
@@ -84,7 +84,7 @@ export function MyAccountOffers(props: MyAccountOffersProps) {
         <OffersList
           offers={activeOffersAsBorrower}
           fields={offerListFields}
-          isLoading={isOffersAsBorrowerLoading || isOffersAsLenderLoading}
+          isLoading={isOffersAsBorrowerLoading && isOffersAsLenderLoading}
           title="Current offers as a borrower"
         />
       </Box>
