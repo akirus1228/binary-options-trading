d3d79aca4211277e31f41cc4cba1ce0976557dc9
Cory Sougstad <cory@hau.me>
2022-08-29 21:21:09 -0700

    FTM-921 Add missing token images

--- libs/shared/images/src/lib/images/tokens/index.ts
+++ libs/shared/images/src/lib/images/tokens/index.ts
@@ -2,5 +2,17 @@ import DaiToken from "./DAI.svg";
 import FHMToken from "./token_OHM.svg";
 import USDBToken from "./USDB.svg";
 import DaiUSDBLP from "./DAI-USDB.svg";
-
-export { DaiToken, FHMToken, USDBToken, DaiUSDBLP };
+import EthToken from "./ETH.svg";
+import UsdcToken from "./USDC.svg";
+import UsdtToken from "./USDT.svg";
+import WbtcToken from "./wBTC.svg";
+export {
+  EthToken,
+  DaiToken,
+  FHMToken,
+  USDBToken,
+  DaiUSDBLP,
+  UsdcToken,
+  UsdtToken,
+  WbtcToken,
+};
