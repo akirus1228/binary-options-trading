73a23452665231e11c54499f3d60642cff275d78
Cory Sougstad <cory@hau.me>
2022-08-30 20:10:11 -0700

    Provide backup value for linear progress to avoid errors.

--- apps/usdb/src/app/pages/balance-vault-details-page/balance-vault-details-page.tsx
+++ apps/usdb/src/app/pages/balance-vault-details-page/balance-vault-details-page.tsx
@@ -193,7 +193,7 @@ export const BalanceVaultDetailsPage = (): JSX.Element => {
                 <Box className="flex fr jf-sb ai-c">
                   <span>{vaultData?.time.completedTime}</span>
                   <LinearProgress
-                    value={vaultData?.time.percentComplete}
+                    value={vaultData?.time.percentComplete ?? 0}
                     variant="determinate"
                     sx={{ width: "60%" }}
                   />
