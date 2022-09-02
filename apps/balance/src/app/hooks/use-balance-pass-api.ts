9f70b5f4bf17737d4fadfebd0c2a898c5eb43fca
Cory Sougstad <cory@hau.me>
2022-09-02 16:43:29 -0700

    Reduce wl check to single call

--- apps/balance/src/app/hooks/use-balance-pass-api.ts
+++ apps/balance/src/app/hooks/use-balance-pass-api.ts
@@ -13,23 +13,15 @@ export const useBPGetProof = (address: string): UseQueryResult<ProofResponse> =>
     ["proof"],
     () => {
       return axios
-        .get(`${balancePassApiUrl}/proof/${address}/0`, {
+        .get(`${balancePassApiUrl}/proof/${address}`, {
           validateStatus: (status) => status < 500,
         })
         .then((res: AxiosResponse) => {
           if (res.status === 200) {
-            return { wl: 1, ...res.data.json };
-          } else {
-            return axios
-              .get(`${balancePassApiUrl}/proof/${address}/1`, {
-                validateStatus: (status) => status < 500,
-              })
-              .then((res) => ({ wl: 2, ...res.data.json }))
-              .catch((err) => {
-                console.warn(err);
-              });
+            return res.data.json;
           }
-        });
+        })
+        .catch((err) => console.warn(err));
     },
 
     { enabled: !!address }
