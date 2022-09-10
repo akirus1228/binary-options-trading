289db0785b5ac2640a133439adda5b1faf0c9946
sacred-danny <sacred.danny.jin@gmail.com>
2022-09-10 18:22:44 +0900

    remove: verified nft validation

--- apps/nft-market/src/app/components/terms-form/terms-form.tsx
+++ apps/nft-market/src/app/components/terms-form/terms-form.tsx
@@ -88,8 +88,6 @@ export const TermsForm = (props: TermsFormProps): JSX.Element => {
   }, 0);
   const preFillPrice = (estimatedPrice || 0) * 0.9;
   const maxFillPrice = (estimatedPrice || 0) * 1.5;
-  const canGetLiquidity =
-    isDev || props.asset?.collection?.safelist_request_status === "verified";
 
   // update term backend api call
   const [
@@ -691,14 +689,7 @@ export const TermsForm = (props: TermsFormProps): JSX.Element => {
         <Button
           variant="contained"
           onClick={handlePermissionRequest}
-          disabled={
-            !canGetLiquidity ||
-            !amount ||
-            !duration ||
-            amount === "0" ||
-            duration === "0" ||
-            !apr
-          }
+          disabled={!amount || !duration || amount === "0" || duration === "0" || !apr}
         >
           Allow Liqd to Access your NFT
         </Button>
@@ -707,14 +698,7 @@ export const TermsForm = (props: TermsFormProps): JSX.Element => {
         <Button
           variant="contained"
           onClick={handleCreateListing}
-          disabled={
-            !canGetLiquidity ||
-            !amount ||
-            !duration ||
-            amount === "0" ||
-            duration === "0" ||
-            !apr
-          }
+          disabled={!amount || !duration || amount === "0" || duration === "0" || !apr}
         >
           Get liquidity
         </Button>
