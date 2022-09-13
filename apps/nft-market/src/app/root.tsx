975cba098004c8ca9feb21293a0d3c184a00b792
sacred-danny <sacred.danny.jin@gmail.com>
2022-09-13 16:08:49 +0900

    fix: scroll issue

--- apps/nft-market/src/app/root.tsx
+++ apps/nft-market/src/app/root.tsx
@@ -1,16 +1,33 @@
+import { Web3ContextProvider } from "@fantohm/shared-web3";
+import { useLayoutEffect } from "react";
 import { Provider } from "react-redux";
+// eslint-disable-next-line node/no-extraneous-import
+import { useLocation } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 
-import { Web3ContextProvider } from "@fantohm/shared-web3";
 import { App } from "./app";
 import store from "./store";
+
+const ScrollToTop = (props: any) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.body.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [location]);
+
+  return <>{props?.children}</>;
+};
 
 const Root = (): JSX.Element => {
   return (
     <Web3ContextProvider>
       <Provider store={store}>
         <BrowserRouter>
-          <App />
+          <ScrollToTop>
+            <App />
+          </ScrollToTop>
         </BrowserRouter>
       </Provider>
     </Web3ContextProvider>
