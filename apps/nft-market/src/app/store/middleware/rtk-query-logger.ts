611306b68d1eba4566c360e809e5b2ed0b516d32
Cory Sougstad <cory@hau.me>
2022-09-28 10:01:46 -0700

    Fix comment

--- apps/nft-market/src/app/store/middleware/rtk-query-logger.ts
+++ apps/nft-market/src/app/store/middleware/rtk-query-logger.ts
@@ -2,9 +2,6 @@ import { isRejectedWithValue } from "@reduxjs/toolkit";
 import type { MiddlewareAPI, Middleware } from "@reduxjs/toolkit";
 import { addAlert, setOpenseaStatus } from "../reducers/app-slice";
 
-/**
- * Log a warning and show a toast!
- */
 export const rtkQueryErrorLogger: Middleware =
   (api: MiddlewareAPI) => (next) => (action) => {
     // RTK Query uses `createAsyncThunk` from redux-toolkit under the hood, so we're able to utilize these matchers!
