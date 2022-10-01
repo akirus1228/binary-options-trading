ddb3322b4f5726f56b788e2efac2a2a16fc9f2e8
trust0212 <kishi.miracledev@gmail.com>
2022-09-30 21:24:25 -0400

    modify ui for lender asset

--- apps/nft-market/src/app/components/asset-list/lender-asset/lender-asset.tsx
+++ apps/nft-market/src/app/components/asset-list/lender-asset/lender-asset.tsx
@@ -275,7 +275,7 @@ export function LenderAsset({ asset }: LenderAssetProps) {
           }}
         >
           <Box
-            className="flex fr fj-sb w100"
+            className={`${style["asset-loan-info"]} w100`}
             sx={{
               alignItems: "top",
               flexFlow: {
@@ -285,7 +285,7 @@ export function LenderAsset({ asset }: LenderAssetProps) {
           >
             <Box
               sx={{
-                width: "50%",
+                flex: 1,
                 flexFlow: "wrap",
                 alignItems: "center",
                 alignContent: "center",
@@ -318,7 +318,7 @@ export function LenderAsset({ asset }: LenderAssetProps) {
             <Box
               className={style["interestElem"]}
               sx={{
-                width: "50%",
+                flex: 1,
                 flexFlow: "wrap",
               }}
             >
