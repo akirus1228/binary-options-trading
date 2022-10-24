ab4dd1ef754e819ebf2ed09b5d41f0e08d6f70ea
ryan9657 <lucky.dev.ryan@gmail.com>
2022-10-25 03:30:08 +0800

    fix: remove some duplicates in merge.

--- apps/nft-market/src/app/types/backend-types.ts
+++ apps/nft-market/src/app/types/backend-types.ts
@@ -422,32 +422,3 @@ export type SendReport = {
   text: string;
   to: string;
 };
-
-export type BlogPostDTO = {
-  id?: string;
-  date: string;
-  blogTitle: string;
-  isFeatured: boolean;
-  blogAsset?: string;
-  blogCategory?: string;
-  content: string;
-  image?: string;
-  seoTitle?: string;
-  seoDescription?: string;
-  seoKeywords?: string;
-  getInTouch?: string;
-};
-export type BlogPostDTO = {
-  id?: string;
-  date: string;
-  blogTitle: string;
-  isFeatured: boolean;
-  blogAsset?: string;
-  blogCategory?: string;
-  content: string;
-  image?: string;
-  seoTitle?: string;
-  seoDescription?: string;
-  seoKeywords?: string;
-  getInTouch?: string;
-};
