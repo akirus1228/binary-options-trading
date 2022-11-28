1c264a52b08e5978ee9310f57e1ef15dbb75976e
ryan9657 <lucky.dev.ryan@gmail.com>
2022-11-29 04:48:08 +0800

    feat: add confirm pop up

--- apps/options/src/app/helpers/data-translations.ts
+++ apps/options/src/app/helpers/data-translations.ts
@@ -1,3 +1,5 @@
+import { TimeframeEnum } from "../core/constants/basic";
+
 export const convertTime = (props: Date): { time: string; date: string } => {
   const time = `${props.getHours()}:${props.getMinutes()}:${props.getSeconds()}`;
   const date = `${props.getDate()}-${props.getMonth() + 1}-${props.getFullYear() - 2000}`;
@@ -16,4 +18,11 @@ export const financialFormatter = new Intl.NumberFormat("en-US", {
 
 export const numberWithCommas = (x: number) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+export const convertTimeString = (e: TimeframeEnum) => {
+  let timeType: any;
+  if (e < 10) timeType = "00:0" + e.toString() + ":00";
+  else timeType = "00:" + e.toString() + ":00";
+  return timeType;
 };
--- apps/options/src/app/pages/trade/components/trading-pad.tsx
+++ apps/options/src/app/pages/trade/components/trading-pad.tsx
@@ -18,17 +18,25 @@ import { useState, useMemo, MouseEvent } from "react";
 
 import { LabelIcon } from "../../../components/label-icon/label-icon";
 import { CurrencyDropdown } from "../../../components/dropdown/currency-dropdown";
-import { fee, double, UnderlyingAssets, TimeframeEnum } from "../../../core/constants/basic";
+import {
+  fee,
+  double,
+  UnderlyingAssets,
+  TimeframeEnum,
+} from "../../../core/constants/basic";
 import { TimeframeDropdown } from "../../../components/dropdown/timeframe-dropdown";
 import { financialFormatter } from "../../../helpers/data-translations";
 import { desiredNetworkId } from "../../../core/constants/network";
+import ConfirmTradePopup from "../../../components/pop-up/confirm-trade";
 
 const TradingPad = () => {
   const { connect, address, connected, chainId } = useWeb3Context();
 
-  const [timeframe, setTimeFrame] = useState<TimeframeEnum>(TimeframeEnum.ONE);
+  const [timeframe, setTimeFrame] = useState(TimeframeEnum.ONE);
   const [tokenAmount, setTokenAmount] = useState<string>("0");
+  const [direction, setDirection] = useState<"Up" | "Down">("Up");
   const [currency, setCurrency] = useState<CurrencyDetails>(currencyInfo["DAI_ADDRESS"]);
+  const [isOpen, setOpen] = useState(false);
 
   const { balance: currencyBalance } = useErc20Balance(
     networks[desiredNetworkId].addresses["DAI_ADDRESS"],
@@ -54,147 +62,164 @@ const TradingPad = () => {
   };
 
   const handleSetting = () => {
-    console.log("click handleSetting");
+    //TODO: handleSetting
   };
 
   const handleUp = () => {
-    console.log("click handleUp");
+    setDirection("Up");
+    setOpen(true);
   };
+
   const handleDown = () => {
-    console.log("click handleDown");
+    setDirection("Down");
+    setOpen(true);
+  };
+
+  const handleClose = (isOpen: boolean) => {
+    setOpen(false);
   };
 
   return (
-    <div className="w-full bg-woodsmoke rounded-3xl xs:px-10 xs:py-10 sm:py-20 md:py-30 sm:px-35 md:px-15 lg:px-35 text-second max-w-500">
-      <div className="trade-header flex justify-between items-center text-22 text-primary mb-25">
-        <p>Trade</p>
-        <SvgIcon
-          component={SettingsOutlined}
-          onClick={handleSetting}
-          className="hidden"
-        />
-      </div>
-      <div className="timeframe rounded-2xl bg-heavybunker px-20 py-10 mb-5">
-        <p className="text-16 mb-10">Timeframe</p>
-        <div>
-          <TimeframeDropdown setTimeframe={setTimeFrame} timeframe={timeframe} />
-        </div>
-      </div>
-      <div className="input rounded-2xl bg-heavybunker px-20 py-10 mb-5">
-        <div className="token-enter flex justify-between items-center xs:mb-10 sm:mb-15">
-          <TextField
-            variant="standard"
-            type="number"
-            InputProps={{
-              disableUnderline: true,
-              style: {
-                flexGrow: 1,
-                fontSize: "26px",
-                margin: "0px",
-              },
-            }}
-            inputProps={{ style: { color: "#c1d6eb" } }}
-            sx={{
-              "& ::-webkit-inner-spin-button": {
-                margin: "0px",
-                appearance: "none",
-              },
-              "& ::-webkit-outer-spin-button": {
-                margin: "0px",
-                appearance: "none",
-              },
-            }}
-            value={tokenAmount}
-            focused
-            onChange={(e) => setTokenAmount(e.target.value || "0")}
+    <>
+      <div className="w-full bg-woodsmoke rounded-3xl xs:px-10 xs:py-10 sm:py-20 md:py-30 sm:px-35 md:px-15 lg:px-35 text-second max-w-500">
+        <div className="trade-header flex justify-between items-center text-22 text-primary mb-25">
+          <p>Trade</p>
+          <SvgIcon
+            component={SettingsOutlined}
+            onClick={handleSetting}
+            className="hidden"
           />
+        </div>
+        <div className="timeframe rounded-2xl bg-heavybunker px-20 py-10 mb-5">
+          <p className="text-16 mb-10">Timeframe</p>
           <div>
-            <CurrencyDropdown
-              setCurrency={setCurrency}
-              selectedCurrency={currency}
-              currencies={UnderlyingAssets}
-            />
+            <TimeframeDropdown setTimeframe={setTimeFrame} timeframe={timeframe} />
           </div>
         </div>
-        <div className="token-status text-16 flex justify-between items-center">
-          <p>{financialFormatter.format(parseFloat(tokenAmount))}</p>
-          <p>
-            Balance:{" "}
-            {currencyBalance &&
-              ethers.utils.formatUnits(currencyBalance, currency?.decimals ?? 18)}
-          </p>
-        </div>
-      </div>
-      <div className="odds rounded-2xl bg-heavybunker px-20 py-10 mb-5">
-        <div className="flex justify-between items-center xs:mb-10 sm:mb-15">
-          <p>Odds</p>
-          <div>
-            <CurrencyDropdown
-              setCurrency={setCurrency}
-              selectedCurrency={currency}
-              currencies={UnderlyingAssets}
+        <div className="input rounded-2xl bg-heavybunker px-20 py-10 mb-5">
+          <div className="token-enter flex justify-between items-center xs:mb-10 sm:mb-15">
+            <TextField
+              variant="standard"
+              type="number"
+              InputProps={{
+                disableUnderline: true,
+                style: {
+                  flexGrow: 1,
+                  fontSize: "26px",
+                  margin: "0px",
+                },
+              }}
+              inputProps={{ style: { color: "#c1d6eb" } }}
+              sx={{
+                "& ::-webkit-inner-spin-button": {
+                  margin: "0px",
+                  appearance: "none",
+                },
+                "& ::-webkit-outer-spin-button": {
+                  margin: "0px",
+                  appearance: "none",
+                },
+              }}
+              value={tokenAmount}
+              focused
+              onChange={(e) => setTokenAmount(e.target.value || "0")}
             />
-          </div>
-        </div>
-        <div className="flex justify-between items-center">
-          <p>1:1</p>
-          <p>Payout:&nbsp;{parseFloat(tokenAmount) * double}</p>
-        </div>
-      </div>
-      <div className="gas flex justify-between items-center xs:px-10 sm:px-20 mb-5">
-        <div className="flex items-center">
-          <LabelIcon
-            label="Platform fee:"
-            icon={ErrorOutlineRounded}
-            reverse
-            backgroundColor="wooksmoke"
-            labelColor="second"
-            iconColor="second"
-          />
-          <p>&nbsp;{fee}%</p>
-        </div>
-        <div className="rounded-2xl p-10 bg-wooksmoke flex items-center">
-          <LabelIcon
-            label="Gas:"
-            icon={LocalGasStationOutlined}
-            reverse
-            backgroundColor="wooksmoke"
-            labelColor="second"
-            iconColor="second"
-          />
-          <p>&nbsp;${(parseFloat(tokenAmount) * fee) / 100}</p>
-        </div>
-      </div>
-      {isWalletConnected ? (
-        hasBalance ? (
-          <div className="action text-white text-center xs:text-20 sm:text-26 cursor-default">
-            <div
-              className="w-full bg-success rounded-2xl xs:py-10 sm:py-15 mb-5"
-              onClick={handleUp}
-            >
-              UP
-            </div>
-            <div
-              className="w-full bg-danger rounded-2xl xs:py-10 sm:py-15"
-              onClick={handleDown}
-            >
-              DOWN
+            <div>
+              <CurrencyDropdown
+                setCurrency={setCurrency}
+                selectedCurrency={currency}
+                currencies={UnderlyingAssets}
+              />
             </div>
           </div>
+          <div className="token-status text-16 flex justify-between items-center">
+            <p>{financialFormatter.format(parseFloat(tokenAmount))}</p>
+            <p>
+              Balance:{" "}
+              {currencyBalance &&
+                ethers.utils.formatUnits(currencyBalance, currency?.decimals ?? 18)}
+            </p>
+          </div>
+        </div>
+        <div className="odds rounded-2xl bg-heavybunker px-20 py-10 mb-5">
+          <div className="flex justify-between items-center xs:mb-10 sm:mb-15">
+            <p>Odds</p>
+            <div>
+              <CurrencyDropdown
+                setCurrency={setCurrency}
+                selectedCurrency={currency}
+                currencies={UnderlyingAssets}
+              />
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <p>1:1</p>
+            <p>Payout:&nbsp;{parseFloat(tokenAmount) * double}</p>
+          </div>
+        </div>
+        <div className="gas flex justify-between items-center xs:px-10 sm:px-20 mb-5">
+          <div className="flex items-center">
+            <LabelIcon
+              label="Platform fee:"
+              icon={ErrorOutlineRounded}
+              reverse
+              backgroundColor="wooksmoke"
+              labelColor="second"
+              iconColor="second"
+            />
+            <p>&nbsp;{fee}%</p>
+          </div>
+          <div className="rounded-2xl p-10 bg-wooksmoke flex items-center">
+            <LabelIcon
+              label="Gas:"
+              icon={LocalGasStationOutlined}
+              reverse
+              backgroundColor="wooksmoke"
+              labelColor="second"
+              iconColor="second"
+            />
+            <p>&nbsp;${(parseFloat(tokenAmount) * fee) / 100}</p>
+          </div>
+        </div>
+        {isWalletConnected ? (
+          hasBalance ? (
+            <div className="action text-white text-center xs:text-20 sm:text-26 cursor-default">
+              <div
+                className="w-full bg-success rounded-2xl xs:py-10 sm:py-15 mb-5"
+                onClick={handleUp}
+              >
+                UP
+              </div>
+              <div
+                className="w-full bg-danger rounded-2xl xs:py-10 sm:py-15"
+                onClick={handleDown}
+              >
+                DOWN
+              </div>
+            </div>
+          ) : (
+            <div className="w-full bg-second text-primary text-center rounded-2xl xs:py-10 sm:py-15 cursor-not-allowed xs:text-18 sm:text-24 cursor-default">
+              Insufficient balance
+            </div>
+          )
         ) : (
-          <div className="w-full bg-second text-primary text-center rounded-2xl xs:py-10 sm:py-15 cursor-not-allowed xs:text-18 sm:text-24 cursor-default">
-            Insufficient balance
-          </div>
-        )
-      ) : (
-        <button
-          className="w-full bg-success rounded-2xl xs:py-10 sm:py-15 text-white text-center xs:text-20 sm:text-26"
-          onClick={onClickConnect}
-        >
-          Connect
-        </button>
-      )}
-    </div>
+          <button
+            className="w-full bg-success rounded-2xl xs:py-10 sm:py-15 text-white text-center xs:text-20 sm:text-26"
+            onClick={onClickConnect}
+          >
+            Connect
+          </button>
+        )}
+      </div>
+      <ConfirmTradePopup
+        interval={timeframe}
+        currencyValue={Number(tokenAmount)}
+        selectedCurrency={currency}
+        direction={direction}
+        open={isOpen}
+        onClose={(value: boolean) => handleClose(value)}
+      />
+    </>
   );
 };
 
