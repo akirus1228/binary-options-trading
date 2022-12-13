69e6a8cee5536760aa54a31c59a7136c828bd48b
ryan9657 <lucky.dev.ryan@gmail.com>
2022-12-14 05:05:19 +0800

    feat: Add Growl component for alert

--- apps/options/src/app/store/reducers/app-slice.ts
+++ apps/options/src/app/store/reducers/app-slice.ts
@@ -9,7 +9,17 @@ interface AppData {
   readonly loading: boolean;
   readonly checkedConnection: boolean;
   readonly underlyingToken: CryptoCurrency;
+  readonly growlNotifications: GrowlNotification[];
 }
+
+export type GrowlNotification = {
+  title?: string;
+  message: string;
+  duration: number;
+  severity?: string;
+  open: boolean;
+  startSeconds: number;
+};
 
 const previousState = loadState("app");
 const initialState: AppData = {
@@ -17,6 +27,7 @@ const initialState: AppData = {
   loading: true,
   checkedConnection: false,
   underlyingToken: Betting_CryptoCurrencies[0],
+  growlNotifications: [],
 };
 
 const appSlice = createSlice({
@@ -32,11 +43,36 @@ const appSlice = createSlice({
     setUnderlyingToken: (state, action: PayloadAction<CryptoCurrency>) => {
       state.underlyingToken = action.payload;
     },
+    addAlert: (state, action: PayloadAction<Partial<GrowlNotification>>) => {
+      const alert = {
+        duration: action.payload.duration || 10000,
+        message: action.payload.message || "",
+        severity: action.payload.severity || "success",
+        startSeconds: Date.now(),
+        title: action.payload.title || "",
+        open: true,
+      } as GrowlNotification;
+      state.growlNotifications = [...state.growlNotifications, alert];
+    },
+    clearAlert: (state, action: PayloadAction<number>) => {
+      state.growlNotifications = [
+        ...state.growlNotifications.map((alert: GrowlNotification) => {
+          if (alert.startSeconds !== action.payload) return alert;
+          return { ...alert, open: false };
+        }),
+      ];
+    },
   },
 });
 
 const baseInfo = (state: RootState) => state.app;
 
 export const appReducer = appSlice.reducer;
-export const { setLoading, setCheckedConnection, setUnderlyingToken } = appSlice.actions;
+export const {
+  setLoading,
+  setCheckedConnection,
+  setUnderlyingToken,
+  addAlert,
+  clearAlert,
+} = appSlice.actions;
 export const getAppState = createSelector(baseInfo, (app) => app);
